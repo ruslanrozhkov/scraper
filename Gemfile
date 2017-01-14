b7cf73f5b9343f9925ebfd1b093bc7e6f912e77e
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-14 14:33:32 +0200

    Install Nokogiri

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 gem 'simple_form'
 
 gem 'devise'
+gem 'validate_url'
+gem 'nokogiri'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :hotness, :numericality => {:allow_blank => true}
+  validates :image_url, :url => {:allow_blank => true}
+
 end
